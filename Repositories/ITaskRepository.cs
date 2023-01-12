5b86d722d9d74c9be56dacc4e3c3d475c595212e
Veskyre <77861827+Veskyre@users.noreply.github.com>
2023-01-12 21:35:38 +0100

    Add files via upload
    Dodano controlery i repo :)

---
+++ Repositories/ITaskRepository.cs
@@ -0,0 +1,20 @@
+﻿using Programowanie_Zaawansowane_Projekt.Models;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace Programowanie_Zaawansowane_Projekt.Repositories
+{
+    public interface ITaskRepository
+    {
+        TaskModel Get(int taskId);
+        IQueryable<TaskModel> GetAllActive();
+        void Add(TaskModel task);
+        void Update(int taskId, TaskModel task);
+        void Delete(int taskId);
+
+
+
+    }
+}
