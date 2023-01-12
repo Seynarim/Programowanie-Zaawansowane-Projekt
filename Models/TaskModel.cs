f57f31ef9332e300be768bb8c2e33a2ae3307706
Seynarim <wojtekvicta@gmail.com>
2023-01-12 20:15:37 +0100

    Dodano Modele Zadań

---
+++ Models/TaskModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Programowanie_Zaawansowane_Projekt.Models
+{
+    [Table("Tasks")]
+    public class TaskModel
+    {
+        [Key]
+
+        public int TaskId { get; set; }
+        [DisplayName("Task Name")]
+        [Required(ErrorMessage = "Name is Required")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [DisplayName("Task Description")]
+        [MaxLength(1500)]
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+
+    }
+}
---
+++ Models/ToDoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Programowanie_Zaawansowane_Projekt.Models
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<TaskModel> Tasks { get; set; }
+    }
+}
