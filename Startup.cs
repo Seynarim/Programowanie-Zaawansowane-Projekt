ad3c31def2646674f6205df946d5357980a3569b
Seynarim <wojtekvicta@gmail.com>
2023-01-12 20:13:42 +0100

    Created Base

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Programowanie_Zaawansowane_Projekt.Models;
+using Programowanie_Zaawansowane_Projekt.Repositories;
 
 namespace Programowanie_Zaawansowane_Projekt
 {
@@ -23,7 +25,18 @@ namespace Programowanie_Zaawansowane_Projekt
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ToDoContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("ToDoDatabase")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //    .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
+            services.AddMvc().AddRazorPagesOptions(options =>
+            {
+                options.Conventions.AddPageRoute("/Task/Index", "");
+            });
+            services.AddTransient<ITaskRepository, TaskRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +45,7 @@ namespace Programowanie_Zaawansowane_Projekt
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
             }
             else
             {
@@ -45,12 +59,16 @@ namespace Programowanie_Zaawansowane_Projekt
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Task}/{action=Index}/{id?}");
+                //   endpoints.MapRazorPages();
             });
         }
+
     }
-}
